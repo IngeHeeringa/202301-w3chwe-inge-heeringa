66e65208375cd9938e153533e6385ade14f34724
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-29 14:40:02 +0100

    Create PageComponent and restructure modules

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,29 +1,9 @@
 import CardListComponent from "./components/CardListComponent/CardListComponent.js";
 import FooterComponent from "./components/FooterComponent/FooterComponent.js";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent.js";
+import PageComponent from "./components/PageComponent/PageComponent.js";
 import { getPokemonInfo, getPokemonList } from "./utils/utils.js";
 
-(async () => {
-  const pokemonListResponse = await getPokemonList(1, 8);
-  let pokemons = [];
-  for (let i = 1; i <= pokemonListResponse.results.length; i++) {
-    const pokemon = getPokemonInfo(i);
-    pokemons.push(pokemon);
-  }
-
-  pokemons = await Promise.all(pokemons);
-
-  const cardList = new CardListComponent(
-    rootContainer,
-    pokemons,
-    pokemonListResponse
-  );
-
-  cardList.render();
-})();
-
 const rootContainer = document.querySelector(".root");
-const header = new HeaderComponent(rootContainer);
-const footer = new FooterComponent(rootContainer);
-header.render();
-footer.render();
+const pageComponent = new PageComponent(rootContainer);
+pageComponent.render();
