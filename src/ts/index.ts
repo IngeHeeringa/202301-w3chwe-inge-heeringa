ef27fcd0c0e308e9995779436257a73a94247e7a
Inge Heeringa <inge.heeringa@gmail.com>
2023-01-29 14:19:16 +0100

    Create FooterComponent

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,8 @@
 import CardListComponent from "./components/CardListComponent/CardListComponent.js";
+import FooterComponent from "./components/FooterComponent/FooterComponent.js";
 import { getPokemonInfo, getPokemonList } from "./utils/utils.js";
 
 (async () => {
-  const rootContainer = document.querySelector(".root");
   const pokemonListResponse = await getPokemonList(1, 8);
   let pokemons = [];
   for (let i = 1; i <= pokemonListResponse.results.length; i++) {
@@ -17,5 +17,10 @@ import { getPokemonInfo, getPokemonList } from "./utils/utils.js";
     pokemons,
     pokemonListResponse
   );
+
   cardList.render();
 })();
+
+const rootContainer = document.querySelector(".root");
+const footer = new FooterComponent(rootContainer);
+footer.render();
